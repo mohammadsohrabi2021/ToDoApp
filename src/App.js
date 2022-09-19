d01088014b95094c5443ea7e234558fe477952bc
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-19 17:57:39 +0430

    add data todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 
 function App() {
-
+  const[ todo , setTodo] = useState([
+    {id:1 ,name:"mohammad" , state:false,dec:'import'},
+    {id:2 ,name:"ali" , state:true,dec:'import'},
+    {id:3 ,name:"reza" , state:false,dec:'import'},
+    {id:4 ,name:"milad" , state:true,dec:'import'},
+])
   return (
     <div>
          
