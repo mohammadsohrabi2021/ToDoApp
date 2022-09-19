3cc5246ab149bccaabd7f3cbdd260a4666cd4166
mohammad sohrabi <mohammadsohrabi141@gmail.com>
2022-09-19 18:22:30 +0430

    init handel delete and handelcheck and create componente todoform and todocard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { useState } from 'react';
 
 function App() {
-  const[ todo , setTodo] = useState([
-    {id:1 ,name:"mohammad" , state:false,dec:'import'},
-    {id:2 ,name:"ali" , state:true,dec:'import'},
-    {id:3 ,name:"reza" , state:false,dec:'import'},
-    {id:4 ,name:"milad" , state:true,dec:'import'},
-])
+  const [todos, setTodos] = useState([
+    { id: 1, name: "mohammad", state: false, dec: 'import' },
+    { id: 2, name: "ali", state: true, dec: 'import' },
+    { id: 3, name: "reza", state: false, dec: 'import' },
+    { id: 4, name: "milad", state: true, dec: 'import' },
+  ])
+  const handeleDelete = id => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+  const handelCheck = id => {
+    setTodos(todos.map(todo => todo.id === id ? { ...todo, state: !todo.state } : todo))
+  }
   return (
-    <div>
-         
+    <div style={{border:'ipx solid #000',margin:'20px'}}>
+      {todos.map(todo => (
+        <div>
+          <div>
+            id:{todo.id}
+          </div>
+          <div>
+            name : {todo.name}
+          </div>
+          <div onClick={()=>handelCheck(todo.id)}>
+            state:{todo.state?'done' :'not done'}
+          </div>
+          <div>
+            des:{todo.dec}
+          </div>
+          <button onClick={() => handeleDelete(todo.id)}>
+            delete
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
